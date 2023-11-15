c24f05604756060a86d4492fd3b01c04527be523
vxhly <pengchengou@gmail.com>
2023-11-15 17:08:53 +0800

    feat(javascript): :sparkles: 新增 @type 声明

--- packages/javascript/index.cjs
+++ packages/javascript/index.cjs
@@ -1,3 +1,8 @@
+/** @type {import('eslint').Linter.BaseConfig} */
+// @ts-check
+
 module.exports = {
+  $schema: 'https://json.schemastore.org/eslintrc.json',
+
   extends: ['@denaro-config/eslint-config-basic', 'eslint:recommended', 'standard']
 }
