e3fd2e13d403568ce6d38f935b5544340c938fe8
vxhly <pengchengou@gmail.com>
2023-11-15 17:06:52 +0800

    feat(eslint-prettier): :sparkles: 新增 @type 声明

--- packages/eslint-prettier/index.cjs
+++ packages/eslint-prettier/index.cjs
@@ -1,6 +1,13 @@
+/** @type {import('eslint').Linter.BaseConfig} */
+// @ts-check
+
 module.exports = {
+  $schema: 'https://json.schemastore.org/eslintrc.json',
+
   extends: ['prettier', 'plugin:prettier/recommended'],
+
   plugins: ['prettier'],
+
   rules: {
     'prettier/prettier': 'error'
   }
