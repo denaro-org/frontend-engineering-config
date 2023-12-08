de3da568c9b0cc4f1b87e8c8257cfa52295161a8
vxhly <pengchengou@gmail.com>
2023-12-08 11:35:50 +0800

    feat(stylelint-less): :sparkles: 新增 前端工程化中的 stylelint 通用配置
    由旧包 @denaro-config/stylelint 中对于 less  规范支持进行拆分成新的依赖包

---
+++ packages/stylelint-less/index.cjs
@@ -0,0 +1,26 @@
+/** @type {import('stylelint').Config} */
+// @ts-check
+
+module.exports = {
+  $schema: 'https://json.schemastore.org/stylelintrc',
+
+  extends: [
+    '@denaro-config/stylelint',
+    'stylelint-config-recommended-less',
+    'stylelint-config-standard-less'
+  ],
+
+  // 不同格式的文件指定自定义语法
+  overrides: [
+    {
+      files: ['**/*.(less|css|vue|html)'],
+      customSyntax: 'postcss-less'
+    },
+    {
+      files: ['**/*.(html|vue)'],
+      customSyntax: 'postcss-html'
+    }
+  ],
+
+  plugins: ['stylelint-less']
+}
