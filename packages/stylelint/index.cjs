49c64d95f4f350a0932b9295bddb9024bd8cd8b3
vxhly <pengchengou@gmail.com>
2023-12-08 10:30:59 +0800

    feat(stylelint): :sparkles: 更新 stylelint 对 scss、sass 语法的支持

--- packages/stylelint/index.cjs
+++ packages/stylelint/index.cjs
@@ -9,17 +9,18 @@ module.exports = {
     'stylelint-config-prettier',
     'stylelint-config-recess-order',
     'stylelint-config-standard',
-    'stylelint-config-standard-scss'
+    'stylelint-config-standard-scss',
+    'stylelint-config-css-modules'
   ],
 
-  plugins: ['stylelint-order', 'stylelint-prettier'],
+  plugins: ['stylelint-scss', 'stylelint-order', 'stylelint-prettier'],
 
   rules: {
     'prettier/prettier': true,
     'order/properties-alphabetical-order': true,
     'at-rule-no-unknown': [
       true,
-      { ignoreAtRules: ['mixin', 'extend', 'content'] }
+      { ignoreAtRules: ['mixin', 'extend', 'content', 'use', 'import'] }
     ]
   }
 }
