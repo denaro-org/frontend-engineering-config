19e362f22973ab169336bdc099cb8a6b7557d176
vxhly <pengchengou@gmail.com>
2023-12-08 11:12:26 +0800

    feat(stylelint): :sparkles: 更新 stylelint 规则
    下一个版本应该拆分 scss、less 的支持包

--- packages/stylelint/index.cjs
+++ packages/stylelint/index.cjs
@@ -5,6 +5,8 @@ module.exports = {
   $schema: 'https://json.schemastore.org/stylelintrc',
 
   extends: [
+    'stylelint-config-recommended-scss',
+    'stylelint-config-standard-vue',
     'stylelint-prettier/recommended',
     'stylelint-config-prettier',
     'stylelint-config-recess-order',
@@ -13,14 +15,155 @@ module.exports = {
     'stylelint-config-css-modules'
   ],
 
+  // 不同格式的文件指定自定义语法
+  overrides: [
+    {
+      files: ['**/*.(scss|css|vue|html)'],
+      customSyntax: 'postcss-scss'
+    },
+    {
+      files: ['**/*.(html|vue)'],
+      customSyntax: 'postcss-html'
+    }
+  ],
+
   plugins: ['stylelint-scss', 'stylelint-order', 'stylelint-prettier'],
 
+  ignoreFiles: [
+    '**/node_modules/**',
+    '**/dist/**',
+    '**/lib/**',
+    '**/*.js',
+    '**/*.jsx',
+    '**/*.tsx',
+    '**/*.ts',
+    '**/*.json',
+    '**/*.md',
+    '**/*.yaml'
+  ],
+
   rules: {
-    'prettier/prettier': true,
-    'order/properties-alphabetical-order': true,
+    'font-family-no-missing-generic-family-keyword': null,
+    'string-quotes': 'single',
+    // 禁止在具有较高优先级的选择器后出现被其覆盖的较低优先级的选择器
+    'no-descending-specificity': null,
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep']
+      }
+    ],
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['deep']
+      }
+    ],
+    'rule-empty-line-before': [
+      'always',
+      { except: ['after-single-line-comment', 'first-nested'] }
+    ],
     'at-rule-no-unknown': [
       true,
-      { ignoreAtRules: ['mixin', 'extend', 'content', 'use', 'import'] }
+      {
+        ignoreAtRules: [
+          'function',
+          'if',
+          'else',
+          'else-if',
+          'each',
+          'include',
+          'mixin',
+          'extend',
+          'content',
+          'use',
+          'import'
+        ]
+      }
+    ],
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['blockless-after-same-name-blockless', 'first-nested'],
+        ignore: ['after-comment'],
+        ignoreAtRules: ['else', 'else-if']
+      }
+    ],
+    // 指定样式的排序
+    'order/properties-order': [
+      'position',
+      'top',
+      'right',
+      'bottom',
+      'left',
+      'z-index',
+      'display',
+      'justify-content',
+      'align-items',
+      'flex-shrink',
+      'float',
+      'clear',
+      'overflow',
+      'overflow-x',
+      'overflow-y',
+      'width',
+      'min-width',
+      'max-width',
+      'height',
+      'min-height',
+      'max-height',
+      'padding',
+      'padding-top',
+      'padding-right',
+      'padding-bottom',
+      'padding-left',
+      'margin',
+      'margin-top',
+      'margin-right',
+      'margin-bottom',
+      'margin-left',
+      'font-size',
+      'font-family',
+      'text-align',
+      'text-justify',
+      'text-indent',
+      'text-overflow',
+      'text-decoration',
+      'white-space',
+      'color',
+      'background',
+      'background-position',
+      'background-repeat',
+      'background-size',
+      'background-color',
+      'background-clip',
+      'border',
+      'border-style',
+      'border-width',
+      'border-color',
+      'border-top-style',
+      'border-top-width',
+      'border-top-color',
+      'border-right-style',
+      'border-right-width',
+      'border-right-color',
+      'border-bottom-style',
+      'border-bottom-width',
+      'border-bottom-color',
+      'border-left-style',
+      'border-left-width',
+      'border-left-color',
+      'border-radius',
+      'opacity',
+      'filter',
+      'list-style',
+      'outline',
+      'visibility',
+      'box-shadow',
+      'text-shadow',
+      'resize',
+      'transition',
+      'content'
     ]
   }
 }
